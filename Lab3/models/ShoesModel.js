821724ef917245f0e38bbb2fd28b4bff9382744d
MysticMaster <dark.mystic.slayer@gmail.com>
2024-03-15 22:39:32 +0700

    Add Lab3

---
+++ Lab3/models/ShoesModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const ShoesSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    idTrademark:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    images: {
+        type: Array,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    describe: {
+        type: String
+    },
+    classify:{
+        type: Number
+    },
+    status: {
+        type: Boolean
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Shoes', ShoesSchema)
